5cc8f0cc5121c964400fa372962ee58d7fd8e40c
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-12-02 20:18:03 +0600

    making a repo for recipe calorise website and make a component called navber

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navber from './component/Navber/Navber'
+
+function App() {
+
+  return (
+    <>
+      <Navber></Navber>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Navber/Navber.jsx
@@ -0,0 +1,10 @@
+
+const Navber = () => {
+    return (
+        <div className="">
+            THis is navber
+        </div>
+    );
+};
+
+export default Navber;
